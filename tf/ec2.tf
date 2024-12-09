483ab5e6ee8fed9c9fd0f23128a9873c6e3c48e7
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 07:05:26 +0900

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -1,9 +1,27 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = toset(["jenkins-x86", "jenkins-grv", "prometheus", "nexus3"])
+  for_each = toset(["jenk-x86", "prome", "nexus3"])
 
   name = "i-${each.key}"
+
+  instance_type          = "c6i.xlarge"
+  key_name               = var.key_pair
+  monitoring             = true
+  vpc_security_group_ids = [module.ec2_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+  associate_public_ip_address	= "true" 
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "ec2_instance_arm" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "i-jenk-arm}"
 
   instance_type          = "c6i.xlarge"
   key_name               = var.key_pair
