feb20c3ea7e4865403ae1c11708d44b01c955d2b
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 07:42:42 +0900

    Update eks.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
 
   cluster_name                    = local.cluster_name
   cluster_version                 = "1.31"
-  cluster_endpoint_private_access = false
+  cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
   cluster_addons = {
@@ -40,3 +40,13 @@ module "eks" {
 }
 
 # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+
+/*
+Error: waiting for EKS Node Group (eks-grv-mig:ng-x86-20241209221503109200000015) create: unexpected state 'CREATE_FAILED', wanted target 'ACTIVE'. last error: i-05f04897353fc76af, i-08cea01975730272b, i-0e21b71b34065f19d: NodeCreationFailure: Instances failed to join the kubernetes cluster
+│
+│   with module.eks.module.eks_managed_node_group["ng-x86"].aws_eks_node_group.this[0],
+│   on .terraform/modules/eks/modules/eks-managed-node-group/main.tf line 392, in resource "aws_eks_node_group" "this":
+│  392: resource "aws_eks_node_group" "this" {
+*/
+
+
