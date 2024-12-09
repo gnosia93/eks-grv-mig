423888b35d906a60bd771791615d08994ce7c39a
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 05:59:12 +0900

    Update sg.tf

--- tf/sg.tf
+++ tf/sg.tf
@@ -1,1 +1,24 @@
-11
+module "ec2_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+# https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
