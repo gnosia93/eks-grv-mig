e9420bb08420965b68f22564e380ec8cabfc2c30
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 06:19:20 +0900

    Update eks.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -28,7 +28,7 @@ module "eks" {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
+      instance_types = ["m6i.2xlarge"]
 
       min_size     = 2
       max_size     = 10
