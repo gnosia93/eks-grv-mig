362419f178a8218dcb508e1561cef39a32f23ec6
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 12:43:26 +0900

    Update eks.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -4,7 +4,7 @@ module "eks" {
 
   cluster_name                    = local.cluster_name
   cluster_version                 = "1.31"
-  cluster_endpoint_private_access = false
+  cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
  # Optional: Adds the current caller identity as an administrator via cluster access entry
