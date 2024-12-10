b54b6b2dcf06e5eb0469b729d76f285ee1bab8d5
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-11 01:34:50 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -2,3 +2,10 @@ variable "key_pair" {
     type = string
     default = "aws-kp-2"                ## 콘솔에서 생성한 키페어 명칭으로 변경.
 }
+
+variable "your_ip_addr" {
+    type = string
+    default = "218.48.121.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
+}
+
+# https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=1&acq=%EB%82%B4%EC%95%84%EC%9D%B4%ED%94%BC&qdt=0&ie=utf8&query=%EB%82%B4%EC%95%84%EC%9D%B4%ED%94%BC
