a479627e03c0f6c05d7d2aed7e0c5de562f311f4
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 18:37:00 +0900

    Update security.tf

--- tf/security.tf
+++ tf/security.tf
@@ -5,19 +5,15 @@ module "ec2_sg" {
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
+  ingress_cidr_blocks      = ["10.10.0.0/16", "0.0.0.0/0"]
+  ingress_rules            = ["ssh-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 8080
       to_port     = 8090
       protocol    = "tcp"
       description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = ["10.10.0.0/16", "0.0.0.0/0"]
     },
   ]
 }
