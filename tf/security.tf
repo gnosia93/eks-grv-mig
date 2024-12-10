b15bc937526b05635bc30d5beacde01411114a01
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 18:38:13 +0900

    Update security.tf

--- tf/security.tf
+++ tf/security.tf
@@ -2,7 +2,6 @@ module "ec2_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks      = ["10.10.0.0/16", "0.0.0.0/0"]
