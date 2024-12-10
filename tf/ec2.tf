f908fcf076ec481e93c57edaf3fcaf943bcbc141
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 19:28:17 +0900

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -44,12 +44,15 @@ module "ec2_instance_x86" {
   associate_public_ip_address	= "true" 
   user_data              = <<_DATA
 #! /bin/bash
-sudo yum install -y python
-sudo yum install -y python3-pip
-sudo yum install -y git
-pip install locust
-pip install faker
-locust -V
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 _DATA
 
 
