d83ce67a97e7751b4bbec37ff0e1cd6ff92b233c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 18:43:59 +0900

    Update security.tf

--- tf/security.tf
+++ tf/security.tf
@@ -4,16 +4,18 @@ module "ec2_sg" {
   name        = "user-service"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks      = ["10.10.0.0/16", "0.0.0.0/0"]
-  ingress_rules            = ["ssh-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 8080
       to_port     = 8090
       protocol    = "tcp"
       description = "User-service ports"
-      cidr_blocks = ["10.10.0.0/16"]
-    }
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {  
+      rule        = "ssh-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
   ]
 }
 # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
