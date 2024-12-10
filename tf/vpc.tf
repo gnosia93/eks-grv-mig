b1b484371a349593209e5b226a0ea760deee3cce
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-10 09:43:43 +0900

    Update vpc.tf

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -6,6 +6,12 @@ locals {
   cluster_name = "eks-grv-mig"
 }
 
+resource "aws_eip" "nat" {
+  count = 1
+
+  vpc = true
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -16,8 +22,14 @@ module "vpc" {
   private_subnets = ["10.0.0.0/22", "10.0.4.0/22"]
   public_subnets  = ["10.0.100.0/22", "10.0.104.0/22"]
 
+  enable_nat_gateway  = false
+  single_nat_gateway  = true
+  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
+
   enable_dns_hostnames = true
   map_public_ip_on_launch = true
+  
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
