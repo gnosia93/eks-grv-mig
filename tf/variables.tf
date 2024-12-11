1784e1f8ab12e0db40c24c0f10487e45ea606f44
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-11 19:07:13 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -8,4 +8,8 @@ variable "your_ip_cidr" {
     default = "218.48.121.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
 }
 
+locals {
+  cluster_name = "eks-grv-adt"
+}
+
 # https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=1&acq=%EB%82%B4%EC%95%84%EC%9D%B4%ED%94%BC&qdt=0&ie=utf8&query=%EB%82%B4%EC%95%84%EC%9D%B4%ED%94%BC
