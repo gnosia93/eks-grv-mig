f440e12dcf1c9a1099ac2cc5411b9a1b18e05a3b
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-11 20:51:17 +0900

    Update eks.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -8,6 +8,19 @@ module "eks" {
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
+  # addon 구성 
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
