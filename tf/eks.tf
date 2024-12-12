c455fce7c08b18146c2677f3784e18c7c07650fc
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-13 08:12:19 +0900

    Update eks.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -14,6 +14,7 @@ module "eks" {
     kube-proxy              = { most_recent = true }
     vpc-cni                 = { most_recent = true }
     eks-pod-identity-agent  = { most_recent = true }
+    aws-ebs-csi-driver      = { most_recent = true }
   }
 
   vpc_id     = module.vpc.vpc_id
