6425b433b90e036b0a90a05b413d0484ecb23297
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-12 13:28:03 +0900

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -31,7 +31,7 @@ data "aws_ami" "ubuntu-x86" {
 module "ec2_instance_x86" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = toset(["nexus3"])
+  for_each = toset(["jenk", "nexus3"])
 
   name = "x86-${each.key}"
 
