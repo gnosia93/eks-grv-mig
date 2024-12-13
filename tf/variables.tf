0f729b4865a5b4d145981b86a71923c8002a8522
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-14 06:49:24 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -5,7 +5,7 @@ variable "key_pair" {
 
 variable "your_ip_cidr" {
     type = string
-    default = "218.48.121.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
+    default = "211.202.61.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
 }
 
 locals {
