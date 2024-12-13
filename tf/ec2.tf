2586e8993233116ec879eb04a1e0f4c886f5fd9f
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-13 12:50:58 +0900

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -60,6 +60,12 @@ sudo apt remove -y awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" ;unzip awscliv2.zip; sudo ./aws/install
 echo "PATH=${PATH}:/usr/local/bin" >> ~/.profile
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl.sha256; chmod +x ./kubectl; sudo mv kubectl /usr/local/bin
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
 _DATA
 
   tags = {
