2467a8e97f2330ee268f9614ddcc123c4053e594
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-15 14:45:27 +0900

    Update security.tf

--- tf/security.tf
+++ tf/security.tf
@@ -37,3 +37,31 @@ module "ec2_sg" {
 
 }
 # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
+
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "add_ec2_sg" {
+    name        = "add_ec2_sg"
+    description = "add_ec2_sg"
+    vpc_id = aws_vpc.eks.id
+
+    ingress = [ 
+        {
+            cidr_blocks = local.all_ingress_ciders 
+            description = "ec2 ingress"
+            from_port = 8080
+            to_port = 9090
+            protocol = "tcp"
+            ipv6_cidr_blocks = [ ]
+            prefix_list_ids = [ "pl-e1a54088" ]
+            security_groups = [ ]
+            self = false
+        },
+       
+    ]
+    
+    tags = {
+        Name = "add_ec2_sg"
+    }   
+}
+
