82ef1b077ceae10e67a63a495bcae5abea9270a8
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2024-12-15 13:38:04 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -10,7 +10,14 @@ variable "your_ip_cidr" {
 
 variable "github_webhook_ips" {
     type = list(string)
-    default = [" ", " "]
+    default = [
+        "192.30.252.0/22",
+        "185.199.108.0/22",
+        "140.82.112.0/20",
+        "143.55.64.0/20",
+        "2a0a:a440::/29",
+        "2606:50c0::/32"
+    ]
     description = "List of IP addresses for github web hook"
 }
 
